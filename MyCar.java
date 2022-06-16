f0ca8e90b79dadcd80ad4f0f7b97138b3d333baf
LogeswariMohan <107361360+LogeswariMohan@users.noreply.github.com>
2022-06-16 10:52:00 +0530

    Add files via upload

---
+++ MyCar.java
@@ -0,0 +1,17 @@
+package week1.day2.assignments;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+		
+		//create new class as MyCar class and create object for existing class and excute
+		
+		Car mc = new Car();
+		System.out.println(mc.applyBreak());
+		System.out.println(mc.applyGear());
+		System.out.println(mc.switchOnAc());
+		System.out.println(mc.applyAcclerate());
+
+	}
+
+}
